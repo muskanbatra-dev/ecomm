d14201495d26135e16a6c4c1f20164e6abca41be
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2024-04-10 23:19:03 +0530

    header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import ProductList from "./pages/ProductList/ProductList";
 import Wishlist from "./pages/Wishlist/Wishlist";
 import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/wishlist" element={<Wishlist />} />
